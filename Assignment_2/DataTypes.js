59d94a1dd2ad4fbb2e5f155c93d602ec27e896ee
Zakir Ullah <zakirullahkhan1713@gmail.com>
2025-08-30 18:53:17 -0700

    Add files via upload

---
+++ Assignment_2/DataTypes.js
@@ -0,0 +1,95 @@
+// ===========================
+// Q1. Number Type
+// ===========================
+
+let marks = 70;
+let totalMarks = 100; 
+console.log(typeof marks, totalMarks);
+
+
+// ===========================
+// Q2. String Type
+// ==========================
+let favoriteFood = "Biryani";
+console.log(favoriteFood);
+console.log(typeof favoriteFood);
+
+// ===========================
+// Q3. Boolean Type
+// ===========================
+
+let isRainy = true;
+console.log(isRainy);
+console.log(typeof isRainy);
+
+// ===========================
+// Q4. Undefined
+// ===========================
+
+let age;
+console.log(age);
+console.log(typeof age);
+
+
+// ===========================
+// Q5. Null
+// ===========================
+
+let emptyBox = null;
+console.log(null);
+console.log(typeof null);
+
+// ===========================
+// Q6. Dynamic Typing
+// ===========================  
+let x = "Hello";
+x = 100;
+console.log(x);
+console.log(typeof x);
+
+// ===========================
+// Q7. typeof Practice
+// ===========================
+
+let name = "Zakir";
+let myAge = 23;
+let isStudent = true;
+let score;
+let data = null;
+
+console.log(typeof name)
+console.log(typeof myAge)
+console.log(typeof isStudent)
+console.log(typeof score)
+console.log(typeof data)
+
+
+// ===========================
+// Q8. String + Number
+// ===========================
+
+let result = "Age: " + 20;
+
+console.log(result);
+console.log(typeof result);
+
+
+
+
+// ===========================
+// Q9. Boolean in String
+// ===========================
+
+let isLoggedIn = true;
+console.log("User is logged in: " + isLoggedIn);
+console.log(typeof isLoggedIn);
+
+// ===========================
+// Q10. Mini Challenge
+// ===========================
+
+let myName = "Zakir";
+let zakirAge = 23;
+let isCodder = true;
+
+console.log("My name is " + myName + ". I am "+ zakirAge + "yeras old. Cooder: " + isCodder)
